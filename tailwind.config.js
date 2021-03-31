b1fe404857b85996650a4528f699d8e153b8ab9f
kirthikishore <76157381+kirthikishore@users.noreply.github.com>
2021-03-31 09:41:22 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+    minWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      full: '100%',
+      36: '143px',
+      28: '119px'
+    }
+  },
+  variants: {},
+  plugins: []
+};
